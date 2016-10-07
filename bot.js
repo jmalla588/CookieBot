0acb77ef4c6afeeee9f983ac09c9bfa97bb0f4c9
Janak Malla <janak.s.malla@vanderbilt.edu>
2016-10-07 02:44:21 -0500

    testing change of activation word

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/cool guy$/;
+      botRegex = /^\/give cookie$/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = cool();
+  botResponse = "Have a cookie!";
 
   options = {
     hostname: 'api.groupme.com',
